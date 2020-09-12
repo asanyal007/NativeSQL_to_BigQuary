a68d0e3c15ffbfdecff1536cb7792c25a1d1f9aa
unknown <sanyalaritra7@gmail.com>
2020-09-12 10:05:47 +0530

    made changes to the regex to inclde some functions

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ from os import listdir
 from os.path import isfile, join
 import function_convert
 
-file_path = r"C:\Workspace\SQL_To_BigQ\Objects_Complexity\Complex_SQL\all_agent_metrics_sf.sql"
+file_path = r"C:\Workspace\SQL_To_BigQ\Objects_Complexity\Complex_SQL\d_all_agent_metrics.sql"
 out_path = r"C:\\Workspace\\SQL_To_BigQ\\Objects_Complexity\\Output_Moderate\\"
 #onlyfiles = [file_path+f for f in listdir(file_path) if isfile(join(file_path, f))]
 sql_file = open(file_path)
 file_name = file_path.split(r"\\")[-1].split('.')[0]
 sql_as_string = sql_file.read()
 
-ptrn = "\s*([a-zA-Z_]\w*[(](\s*[a-zA-Z_]\w*[(]|[^()]+[)]|[)]))"
+ptrn = "\s*([a-zA-Z_]\w*[(](\s*[a-zA-Z_]\w*|[^()]+[)]|[)])[)])"
 functions_in_file = re.findall(ptrn, sql_as_string)
 for matches in functions_in_file:
-    #print(matches)
-    print(matches, function_convert.map_function(matches[0]))
+    print(matches)
+    print( function_convert.map_function(matches[0]))
 
