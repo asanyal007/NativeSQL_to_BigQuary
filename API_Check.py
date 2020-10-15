8bdd3c576926a06e89c302b67c64797342948684
unknown <sanyalaritra7@gmail.com>
2020-10-15 16:28:14 +0530

    added query stats

--- API_Check.py
+++ API_Check.py
@@ -1,10 +1,14 @@
 from google.cloud import bigquery
 from google.oauth2 import service_account
 from google.api_core.exceptions import ClientError
+import subprocess
+import os
+import sqlvalidator
+
 def API_check(str_sql):
     credentials = service_account.Credentials.from_service_account_file('enhanced-idiom-287811-7deab48e37f8.json')
-
     project_id = 'enhanced-idiom-287811'
+
     client = bigquery.Client(credentials= credentials,project=project_id)
     job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
     try:
@@ -13,6 +17,27 @@ def API_check(str_sql):
         return results
     except ClientError as err:
         error_dic = err.__dict__['_errors']
-        return error_dic
+        return error_dic,err
 
+
+def get_stats(sql_str):
+    credentials = service_account.Credentials.from_service_account_file('enhanced-idiom-287811-7deab48e37f8.json')
+    project_id = 'enhanced-idiom-287811'
+    # Construct a BigQuery client object.
+    client = bigquery.Client(credentials = credentials, project=project_id)
+
+    job_config = bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
+
+    # Start the query, passing in the extra configuration.
+    query_job = client.query(
+        (
+            "{}".format(sql_str)
+        ),
+        job_config=job_config,
+    )  # Make an API request.
+
+    # A dry run query completes immediately.
+    return query_job
+
+print(get_stats("select * from `enhanced-idiom-287811.prod.INFORMATION_SCHEMA.TABLES`").total_bytes_processed)
 
